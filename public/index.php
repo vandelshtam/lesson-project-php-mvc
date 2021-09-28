f95753f754af782544dcf1c7b5e15a06cd9a54b2
vandelshtam <mvlju977@gmail.com>
2021-09-28 14:26:26 +0300

    Первое сохранение нового проекта  на "чистом"  php-на классах со структурой mvc

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+//include __DIR__ .'/../function.php';
+echo 123; exit;
+$routes = [
+    "/php/lessons_php/module_1/home" => 'functions/homepage.php',
+    "/php/lessons_php/module_1/about" => 'functions/about.php',
+    "/php/lessons_php/module_1/edit.php" => 'functions/about.php'
+];
+
+$route = $_SERVER['REQUEST_URI'];
+
+if(array_key_exists($route, $routes))
+{
+    //dd($routes[$route]);
+    include '/Applications/MAMP/htdocs/php/lessons_php/module_1/'.$routes[$route];exit;
+}
+else
+{
+    dd(404);
+}
+
